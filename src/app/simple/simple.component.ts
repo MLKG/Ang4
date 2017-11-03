2a5fcdd738fee93019ed85fe6cc087e7c0b5257e
MLKG <1059158548@qq.com>
2017-11-03 14:09:20 +0800

    update

--- src/app/simple/simple.component.ts
+++ src/app/simple/simple.component.ts
@@ -7,18 +7,44 @@ import {Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
       {{message}}
     </p>
     <div>
-        <input #myInput type="text" (keydown.enter)="onEnter($event, myInput.value)" [(ngModel)]="message" style="border: 1px solid red;">
+        <input #myInput
+        type="text"
+        (keydown.enter)="onEnter($event, myInput.value)"
+        [(ngModel)]="message"
+        [ngClass]="{mousedown: isMousedown}"
+        >
         <!--[(ngModel)] 双向数据绑定--><!--[ngModel] 单向数据绑定-->
         <button (click)="update.emit({text: message})">更新</button>
+        <div [ngStyle]="{color: 'white', 'background-color': 'black'}"
+             [style.border-top]="'1px solid blue'"
+             [style.font-size.px]="fontSize"
+        >Uses fixed white text on blue background</div>
     </div>
   `,
-  styles: []
+  styles: [`
+    :host {
+      margin: 10px;
+      display: inline-block;
+    }
+    .mousedown {
+      border: 2px solid red;
+    }
+    input:focus {
+      color: blue;
+    }
+  `]
+  /* :host 表示选择的是宿主元素,即组件模板中的app-simple元素 */
 })
 export class SimpleComponent implements OnInit {
+  isMousedown: boolean;
+  fontSize: number;
   @Input() message: string;
   @Input() index: number;
   @Output() update = new EventEmitter<{text: string}>();
-  constructor() {}
+  constructor() {
+    this.isMousedown = true;
+    this.fontSize = 12;
+  }
 
   onClick(event, value) {
     console.log(event);
