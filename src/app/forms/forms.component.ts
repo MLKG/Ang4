8472399c579be75ef9e1ff8ffd15e03fd0d01f10
MLKG <1059158548@qq.com>
2017-11-06 11:38:12 +0800

    update

--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -3,14 +3,33 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-forms',
   templateUrl: './forms.component.html',
-  styles: []
+  styles: [`
+    input.ng-invalid {
+      border: 3px solid red;
+    }
+    input.ng-valid {
+      border: 3px solid green;
+    }
+    fieldset {
+      border: none;
+      padding: 0;
+      margin: 0;
+    }
+    select.ng-invalid + label:after {
+      content: '<-- 请选择版本!'
+    }
+  `]
 })
 export class FormsComponent implements OnInit {
-  username = 'Da@qq.com';
+  username = 'Lin@qq.com';
+  versions = ['1.X', '2.X', '3.X'];
+  books = ['Ang1', 'Ang2', 'Ang3'];
+  versionName = '2.X';
+  onSubmit(value) {
+    console.log(value);
+  }
   constructor() {
   }
-
   ngOnInit() {
   }
-
 }
