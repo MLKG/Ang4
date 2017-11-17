b7aee00c005e40e861dd1be377db34c74794c18e
MLKG <1059158548@qq.com>
2017-11-17 16:07:37 +0800

    第三章：第9节课改造在线竞拍应用

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule} from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -10,6 +11,13 @@ import { SearchComponent } from './search/search.component';
 import { CarouselComponent } from './carousel/carousel.component';
 import { ProductComponent } from './product/product.component';
 import { StarsComponent } from './stars/stars.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { HomeComponent } from './home/home.component';
+
+const routeConfig: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'product/:productTitle', component: ProductDetailComponent }
+];
 
 @NgModule({
   // 只能声明组件，指令，管道
@@ -20,14 +28,17 @@ import { StarsComponent } from './stars/stars.component';
     SearchComponent,
     CarouselComponent,
     ProductComponent,
-    StarsComponent
+    StarsComponent,
+    ProductDetailComponent,
+    HomeComponent
   ],
   // 声明模块，加载完模块(注入模块)之后，
   // 会在index.html寻找启动模块指定的主组件对应的CSS选择器，并加载对应的模板进行渲染
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(routeConfig)
   ],
   // 声明服务
   providers: [],
