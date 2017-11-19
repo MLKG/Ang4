7be89f6989f7a47edf57193bcfcc2a5326fb2c51
曾桂林 <zengguilin@pywm.com.cn>
2017-11-19 21:44:40 +0800

    第四章：第4节使用工厂和值声明提供器

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import {Product1Component} from './product1/product1.component';
 import {ProductService} from './shared/product.service';
 import {Product2Component} from './product2/product2.component';
 import {LoggerService} from './shared/logger.service';
+import {AnotherProductService} from './shared/another-product.service';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,22 @@ import {LoggerService} from './shared/logger.service';
   imports: [
     BrowserModule
   ],
-  providers: [ProductService, LoggerService],
+  providers: [{
+    provide: ProductService, useFactory: (logger: LoggerService, appConfig) => {
+      console.log(logger);
+      console.log(appConfig);
+      const dev = Math.random() > 0.5;
+      console.log(dev);
+      if (appConfig.isDev) {
+        return new ProductService(logger);
+      } else {
+        return new AnotherProductService(logger);
+      }
+    },
+    deps: [LoggerService, 'APP_CONFIG']
+  }, LoggerService, {
+    provide: 'APP_CONFIG', useValue: { isDev: false }
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
