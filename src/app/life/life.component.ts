259bce48c39554ecf258ca983918af1b3af987dd
曾桂林 <zengguilin@pywm.com.cn>
2017-11-24 22:17:07 +0800

    第6章：第5节组件的生命周期钩子函数概述

--- src/app/life/life.component.ts
+++ src/app/life/life.component.ts
@@ -11,6 +11,7 @@ import {
   Input,
   SimpleChanges
 } from '@angular/core';
+// 初始化logIndex 为1；
 let logIndex = 1;
 @Component({
   selector: 'app-life',
@@ -26,43 +27,39 @@ export class LifeComponent implements
   AfterViewInit,
   AfterViewChecked,
   OnDestroy {
+  // 输入属性titleName
   @Input()
-  name: string;
-  @Input()
-  stock: string;
+  titleName: string;
+  // 定义一个打印日志的方法
   logIt (msg: string) {
-    console.log(`#${logIndex++} ${msg}`);
+    console.log(`#${logIndex++} ${msg}`);  // #1 ngDoCheck
   }
+  // 构造函数
   constructor() {
-    this.logIt('name属性在constructor里的值是：' + name);
+    console.log(this.titleName);
+    this.logIt('name属性在constructor里的值是：' + this.titleName);
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    this.logIt('titleName属性在ngOnChanges里的值是：' + changes['titleName'].currentValue);
   }
   ngOnInit(): void {
     this.logIt('ngOnInit');
   }
-  ngOnChanges(changes: SimpleChanges): void {
-    const name = changes['name'].currentValue;
-    this.logIt('name属性在constructor里的值是：' + name);
-  }
   ngDoCheck(): void {
     this.logIt('ngDoCheck');
   }
-
   ngAfterContentInit(): void {
     this.logIt('ngAfterContentInit');
   }
-
   ngAfterContentChecked(): void {
     this.logIt('ngAfterContentChecked');
   }
-
   ngAfterViewInit(): void {
     this.logIt('ngAfterViewInit');
   }
-
   ngAfterViewChecked(): void {
     this.logIt('ngAfterViewChecked');
   }
-
   ngOnDestroy(): void {
     this.logIt('ngOnDestroy');
   }
