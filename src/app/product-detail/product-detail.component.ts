d0ad784969333d47126aa323cf78343278dc39e1
MLKG <1059158548@qq.com>
2017-11-27 16:01:12 +0800

    第6章：第10节课 投影钩子（ngAfterContentInit, ngAfterContentChecked）, 路由变动时的销毁钩子ngOnDestroy

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -1,19 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute, Params} from '@angular/router';
 import {Product, Comment, ProductService} from '../shared/product.service';
+import {DatePipe} from '@angular/common';
 
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
-  styleUrls: ['./product-detail.component.css']
+  styleUrls: ['./product-detail.component.css'],
+  providers: [DatePipe]
 })
 export class ProductDetailComponent implements OnInit {
 
   private imgUrl = 'http://placehold.it/820x230';
   product: Product;
   comments: Comment[];
+  newRating = 5;
+  newComment = '';
+  isCommentHidden = true;
   constructor(private routerInfo: ActivatedRoute,
-              private productService: ProductService) { }
+              private productService: ProductService,
+              private datePipe: DatePipe) { }
 
   ngOnInit() {
     // this.routerInfo.params.subscribe((params: Params) => {
@@ -24,5 +30,21 @@ export class ProductDetailComponent implements OnInit {
     const productId = Number(this.routerInfo.snapshot.params['productId']);
     this.product = this.productService.getProduct(productId);
     this.comments = this.productService.getCommentsForProductId(productId);
+  }
+  addComment() {
+    const comment = new Comment(
+      0,
+      this.product.id,
+      this.datePipe.transform(new Date(), 'y-MM-dd HH:mm:ss'),
+      '哒林',
+      this.newRating,
+      this.newComment
+    );
+    this.comments.unshift(comment);
+    const sumTotal = this.comments.reduce((sum, item) => sum + item.rating, 0);
+    this.product.rating = sumTotal / this.comments.length;
+    this.newComment = '';
+    this.newRating = 5;
+    this.isCommentHidden = true;
   }
 }
--- src/app/stars/stars.component.ts
+++ src/app/stars/stars.component.ts
@@ -1,21 +1,31 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {Component, OnInit, Input, Output, EventEmitter, OnChanges, SimpleChanges} from '@angular/core';
 
 @Component({
   selector: 'app-stars',
   templateUrl: './stars.component.html',
   styleUrls: ['./stars.component.css']
 })
-export class StarsComponent implements OnInit {
-
+export class StarsComponent implements OnInit, OnChanges {
   @Input()
   private rating = 0;
+  @Output()
+  private ratingChange: EventEmitter<number> = new EventEmitter();
+  // 输出属性是输入属性的名字 + ‘Change’的时候，在父组件可用自己用[(rating)]去进行双向绑定
   private stars: boolean[];
+  @Input()
+  private readonly = true;
   constructor() { }
-
-  ngOnInit() {
+  ngOnChanges(changes: SimpleChanges): void {
     this.stars = [];
     for (let i = 1; i <= 5; i++) {
       this.stars.push(i > this.rating);
+    }
+  }
+  ngOnInit() {}
+  clickStar(index: number) {
+    if (!this.readonly) {
+      this.rating = index + 1;
+      this.ratingChange.emit(this.rating);
     }
   }
 
