cac3ef52e3890cc6aeaf86d393db572dca33b6a2
MLKG <1059158548@qq.com>
2017-11-29 16:58:16 +0800

    第七章：第5节 响应式表单重构

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { TemplateFormComponent } from './template-form/template-form.component';
 import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
+import { ReactiveRegistComponent } from './reactive-regist/reactive-regist.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     TemplateFormComponent,
-    ReactiveFormComponent
+    ReactiveFormComponent,
+    ReactiveRegistComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/reactive-regist/reactive-regist.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-reactive-regist',
+  templateUrl: './reactive-regist.component.html',
+  styleUrls: ['./reactive-regist.component.css']
+})
+export class ReactiveRegistComponent implements OnInit {
+
+  formModel: FormGroup;
+  constructor(fb: FormBuilder) {
+    this.formModel = fb.group({
+      username: [''],
+      mobile: [''],
+      passwordsGroup: fb.group({
+        password: [''],
+        pconfirm: ['']
+      })
+    }, {});
+  }
+  onSubmit() {
+    console.log(this.formModel.value);
+  }
+  ngOnInit() {
+  }
+
+}
