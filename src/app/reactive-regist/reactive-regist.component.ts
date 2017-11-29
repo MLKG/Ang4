fd260660670484aa47f70822f7eb1d4012a37898
MLKG <1059158548@qq.com>
2017-11-29 17:33:22 +0800

    第七章：第6节 响应式表单校验上

--- src/app/reactive-regist/reactive-regist.component.ts
+++ src/app/reactive-regist/reactive-regist.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import {AbstractControl, FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-regist',
@@ -7,19 +7,35 @@ import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
   styleUrls: ['./reactive-regist.component.css']
 })
 export class ReactiveRegistComponent implements OnInit {
-
   formModel: FormGroup;
+  mobileValidator(control: FormControl): any {
+    const phoneReg = /^1[3-9]\d{9}$/;
+    const vaild = phoneReg.test(control.value);
+    console.log('mobile的校验结果是' + vaild);
+    return vaild ? null : { mobile: true };
+  }
+  equalValidator(group: FormGroup): any {
+    const password: FormControl = group.get('password') as FormControl;
+    const pconfirm: FormControl = group.get('pconfirm') as FormControl;
+    const valid: boolean = (password.value === pconfirm.value);
+    console.log('交易密码的校验结果是' + valid);
+    return valid ? null : { equal: true };
+  }
   constructor(fb: FormBuilder) {
     this.formModel = fb.group({
-      username: [''],
-      mobile: [''],
+      username: ['', [Validators.required, Validators.minLength(6)]],
+      mobile: ['', this.mobileValidator],
       passwordsGroup: fb.group({
         password: [''],
         pconfirm: ['']
-      })
-    }, {});
+      }, {validator: this.equalValidator})
+    });
   }
   onSubmit() {
+    const isValid: boolean = this.formModel.get('username').valid;
+    console.log('username的校验结果' + isValid);
+    const errors: any = this.formModel.get('username').errors;
+    console.log('username的错误信息是' + JSON.stringify(errors));
     console.log(this.formModel.value);
   }
   ngOnInit() {
