d4419a07c11149630e7e1329bdac036834adb58c
MLKG <1059158548@qq.com>
2017-11-30 11:28:21 +0800

    第七章：第9节 模板式表单校验

--- src/app/template-form/template-form.component.ts
+++ src/app/template-form/template-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-template-form',
@@ -8,12 +9,21 @@ import { Component, OnInit } from '@angular/core';
 export class TemplateFormComponent implements OnInit {
 
   private userName = 'Dalin';
+  mobileValid = true;
+  mobileUntoched = true;
   constructor() { }
 
   ngOnInit() {
   }
-  onSubmit(value: any) {
+  onSubmit(value: any, valid: boolean) {
     console.log(value);
+    console.log(valid);
+  }
+  onMobileInput(form: NgForm) {
+    if (form) {
+      this.mobileValid = form.form.get('mobile').valid;
+      this.mobileUntoched = form.form.get('mobile').untouched;
+    }
   }
 
 }
